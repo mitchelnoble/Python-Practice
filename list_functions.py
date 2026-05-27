038a08e6c44cadb2ea4e4dc30567164f7aa62361
mitchel noble <mitchelparkernoble@gmail.com>
2026-05-27 12:51:23 -0700

    list functions and sets

---
+++ list_functions.py
@@ -0,0 +1,5 @@
+my_list = [1,2,3]
+my_list.append(4) #adds 4 to the end
+my_list.insert(1,10) #inserts 10 at index 1
+my_list.remove(2) #removes the first occurence of 2
+print(my_list) #Output: [1,10,3,4]
---
+++ set.py
@@ -0,0 +1,9 @@
+#a way to store unique values.
+#they are unordered and do not allow duplicates.
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+fruits.discard("banana")
+print(fruits)
+
+#also have set specific functions (union, intersection, difference) can directly perform mathematical set operations (for data processing tasks)
