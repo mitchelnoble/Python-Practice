d89afab5c0952aa691800c338a6f98086dba702f
mitchel noble <mitchelparkernoble@gmail.com>
2026-06-14 01:41:28 -0700

    common types of python errors

---
+++ runtime_errors.py
@@ -0,0 +1,14 @@
+#Common errors in Python
+
+#SyntaxError: the code violates Python's syntax rules
+
+#   print("Hello World"  #Missing closing parentesis
+
+#TypeError: an operation is applied to an inappropriate object 
+
+#   print("Hello" + 5) #Cannot concatenate str and int
+
+#ValueError: a function receives an argument of the correct type, but an invalid value
+
+#   int("abc") #Cannot convert string to int
+
